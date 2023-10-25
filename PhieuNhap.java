fcf242d69a6bcba48f892c936bd6ab2ed77f053b
TDKhoa2712 <147808726+TDKhoa2712@users.noreply.github.com>
2023-10-25 07:26:23 +0700

    Create PhieuNhap.java

---
+++ PhieuNhap.java
@@ -0,0 +1,42 @@
+public class PhieuNhap{
+    private String maPN;
+    private String ngay;
+    private String maNV;
+    private String maNCC;
+    private long tongtien;
+
+    public String getMaPN(){
+        return this.maPN;
+    }
+    public void setMaPN(String maPN){
+        this.maPN = maPN;
+    }
+
+    public String getNgay(){
+        return this.ngay;
+    }
+    public void setNgay(String ngay){
+        this.ngay = ngay;
+    }
+
+    public String getMaNV(){
+        return this.maNV;
+    }
+    public void setMaNV(String maNV){
+        this.maNV = maNV;
+    }
+
+    public String getMaNCC(){
+        return this.maNCC;
+    }
+    public void setMaNCC(String maNCC){
+        this.maNCC = maNCC;
+    }
+
+    public long getTongtien(){
+        return this.tongtien;
+    }
+    public void setTongtien(long tongtien){
+        this.tongtien = tongtien;
+    }
+}
