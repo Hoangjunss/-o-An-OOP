8c61d00f04baf741b5e31b6f533f8e1ae420b4dc
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2023-10-25 13:59:06 +0700

    Update DanhSachSanPham.java

--- DanhSachSanPham.java
+++ DanhSachSanPham.java
@@ -8,29 +8,32 @@ public class DanhSachSanPham {
     public DanhSachSanPham(){}
 
     public void Nhap(){
-        //Co can tao them 1 danh sach phu kien khong(bao gom cac thuoc tinh phu kien da chon)
-        //Danh sach chi co san pham
-        //Doi voi thong tin. co can Set san toan bo thong tin khong.
-        //Cach de hien thi so luong muc da chon de xem chi tiet
-        //Chi tiet SP?
         System.out.print("-|Nhap so luong san pham: "); N=sc.nextInt();
         SanPham[] SP = new SanPham[N];
         int CHOOSE;
         for(int i=0; i<SP.length; i++){
             System.out.println("1./ San Pham May Tinh");
             System.out.println("2./ San Pham Lap Top");
+            System.out.println("3./ San Pham Phu Kien");
             System.out.print("Chon: "); CHOOSE = sc.nextInt();
             switch(CHOOSE){
                 case 1:
                     SP[i] = new MayTinh();
                     SP[i].Nhap();
+                    break;
                 case 2:
                     SP[i] = new LapTop();
                     SP[i].Nhap();
+                    break;
+                case 3: 
+                    SP[i] = new SanPham();
+                    SP[i].Nhap();
+                    break;
                 default: System.out.println("Chon Lai");
             }
         }
     }
+    
     public void Xuat(){
         for(int i=0; i<SP.length; i++){
             if(SP[i] instanceof MayTinh) SP[i].Xuat();
