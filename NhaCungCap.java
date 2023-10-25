10a48c6a9a21c04fcfee8dd36896d838a955689e
TDKhoa2712 <147808726+TDKhoa2712@users.noreply.github.com>
2023-10-25 07:26:09 +0700

    Create NhaCungCap.java

---
+++ NhaCungCap.java
@@ -0,0 +1,34 @@
+public class NhaCungCap {
+    private String maNCC;
+    private String ten;
+    private String diachi;
+    private int sdt;
+
+    public String getMaNCC(){
+        return this.maNCC;
+    }
+    public void setMaNCC(String maNCC){
+        this.maNCC=maNCC;
+    }
+
+    public String getTen(){
+        return this.ten;
+    }
+    public void setTen(String ten){
+        this.ten=ten;
+    }
+
+    public String getDiachi(){
+        return this.diachi;
+    }
+    public void setDiachi(String diachi){
+        this.diachi=diachi;
+    }
+
+    public int getSdt(){
+        return this.sdt;
+    }
+    public void setSdt(int sdt){
+        this.sdt=sdt;
+    }
+}
