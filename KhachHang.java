3597f142141d6fd38d6818c8dfd5234ad858dcb0
TrTrK03 <148329868+TrTrK03@users.noreply.github.com>
2023-10-25 07:54:17 +0700

    Add files via upload

---
+++ KhachHang.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+public class KhachHang {
+    private String makh;
+    private String hokh;
+    private String tenkh;
+    private int sdt;
+    private String diachi;
+
+    public KhachHang(String makh, String hokh, String tenkh, int sdt, String diachi) {
+        this.manv = manv;
+        this.ho = ho;
+        this.ten = ten;
+        this.sdt = sdt;
+        this.diachi = diachi;
+    }
+
+    public String getMakh() {
+        return makh;
+    }
+
+    public void setMakh(String makh) {
+        this.manv = makh;
+    }
+
+    public String getHokh() {
+        return hokh;
+    }
+
+    public void setHokh(String hokh) {
+        this.ho = hokh;
+    }
+
+    public String getTenkh() {
+        return tenkh;
+    }
+
+    public void setTenkh(String tenkh) {
+        this.ten = tenkh;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(int sdt) {
+        this.sdt = sdt;
+    }
+
+    public String getDiachi() {
+        return diachi;
+    }
+
+    public void setDiachi(String diachi) {
+        this.diachi = diachi;
+    }
+
+    public void nhap() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập mã khách hàng: ");
+        this.makh = scanner.nextLine();
+        System.out.print("Nhập họ khách hàng: ");
+        this.hokh = scanner.nextLine();
+        System.out.print("Nhập tên khách hàng: ");
+        this.tenkh = scanner.nextLine();
+        System.out.print("Nhập số điện thoại khách hàng: ");
+        this.sdt = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Nhập địa chỉ khách hàng: ");
+        this.diachi = scanner.nextLine();
+    }
+
+    public void xuat() {
+        System.out.println("Mã khách hàng: " + this.makh);
+        System.out.println("Họ khách hàng: " + this.hokh);
+        System.out.println("Tên khách hàng: " + this.tenkh);
+        System.out.println("Số điện thoại khách hàng: " + this.sdt);
+        System.out.println("Địa chỉ khách hàng: " + this.diachi);
+    }
+}
---
+++ NhanVien.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+public class NhanVien {
+    private String manv;
+    private String ho;
+    private String ten;
+    private int sdt;
+    private String diachi;
+
+    public NhanVien(String manv, String ho, String ten, int sdt, String diachi) {
+        this.manv = manv;
+        this.ho = ho;
+        this.ten = ten;
+        this.sdt = sdt;
+        this.diachi = diachi;
+    }
+
+    public String getManv() {
+        return manv;
+    }
+
+    public void setManv(String manv) {
+        this.manv = manv;
+    }
+
+    public String getHo() {
+        return ho;
+    }
+
+    public void setHo(String ho) {
+        this.ho = ho;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(int sdt) {
+        this.sdt = sdt;
+    }
+
+    public String getDiachi() {
+        return diachi;
+    }
+
+    public void setDiachi(String diachi) {
+        this.diachi = diachi;
+    }
+
+    public void nhap() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập mã nhân viên: ");
+        this.manv = scanner.nextLine();
+        System.out.print("Nhập họ nhân viên: ");
+        this.ho = scanner.nextLine();
+        System.out.print("Nhập tên nhân viên: ");
+        this.ten = scanner.nextLine();
+        System.out.print("Nhập số điện thoại nhân viên: ");
+        this.sdt = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Nhập địa chỉ nhân viên: ");
+        this.diachi = scanner.nextLine();
+    }
+
+    public void xuat() {
+        System.out.println("Mã nhân viên: " + this.manv);
+        System.out.println("Họ nhân viên: " + this.ho);
+        System.out.println("Tên nhân viên: " + this.ten);
+        System.out.println("Số điện thoại nhân viên: " + this.sdt);
+        System.out.println("Địa chỉ nhân viên: " + this.diachi);
+    }
+}
