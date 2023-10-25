ce824bc9812ec410d991ed55cd6e3bf7e914dd04
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2023-10-25 13:37:05 +0700

    Update LapTop.java

--- LapTop.java
+++ LapTop.java
@@ -2,23 +2,23 @@
 
 public class LapTop extends SanPham {
     private String Pin;
-    private Float Cannang;
-    private String Webcam;
-    private Float Kichthuoc;
+    private Float CanNang;
+    private String WebCam;
+    private Float KichThuoc;
     public LapTop(){}
     public LapTop(String Masp, String Tensp, int Sl, long Dongia, String Donvitinh, String Ocung, String Manhinh, String Phim, String Chuot, String Loa, String Pin, Float Cannang, String Webcam, Float Kichthuoc){
         super(Masp, Tensp, Sl, Dongia, Donvitinh, Ocung, Manhinh, Phim, Chuot, Loa);
         this.Pin = Pin;
-        this.Cannang = Cannang;
-        this.Webcam = Webcam;
-        this.Kichthuoc = Kichthuoc;
+        this.CanNang = Cannang;
+        this.WebCam = Webcam;
+        this.KichThuoc = Kichthuoc;
     }
     public LapTop(LapTop LT){
         super((SanPham)LT);
         Pin = LT.Pin;
-        Cannang = LT.Cannang;
-        Webcam = LT.Webcam;
-        Kichthuoc = LT.Kichthuoc;
+        CanNang = LT.CanNang;
+        WebCam = LT.WebCam;
+        KichThuoc = LT.KichThuoc;
     }
     public String GetPin(){
         return Pin;
@@ -27,37 +27,37 @@ public class LapTop extends SanPham {
         this.Pin = Pin;
     }
     public Float GetCanNang(){
-        return Cannang;
+        return CanNang;
     }
     public void SetCanNang(Float Cannang){
-        this.Cannang = Cannang;
+        this.CanNang = Cannang;
     }
     public String GetWebCam(){
-        return Webcam;
+        return WebCam;
     }
     public void SetWebCam(String Webcam){
-        this.Webcam = Webcam;
+        this.WebCam = Webcam;
     }
-    public Float GetKichthuoc(){
-        return Kichthuoc;
+    public Float GetKichThuoc(){
+        return KichThuoc;
     }
-    public void SetKichthuoc(Float Kichthuoc){
-        this.Kichthuoc = Kichthuoc;
+    public void SetKichThuoc(Float Kichthuoc){
+        this.KichThuoc = Kichthuoc;
     }
 
     @Override public void Nhap(){
         super.Nhap();
         System.out.print("Pin: "); Pin = sc.nextLine();
-        System.out.print("Can Nang: "); Cannang = sc.nextFloat();
-        System.out.print("WebCam: "); Webcam = sc.nextLine();
-        System.out.print("Kich thuoc: "); Kichthuoc = sc.nextFloat();
+        System.out.print("Can Nang: "); CanNang = sc.nextFloat();
+        System.out.print("WebCam: "); WebCam = sc.nextLine();
+        System.out.print("Kich thuoc: "); KichThuoc = sc.nextFloat();
     }
     @Override public void Xuat(){
         System.out.println("*Thong Tin Lap Top*");
         super.Xuat();
         System.out.println("Pin: "+Pin);
-        System.out.println("Can Nang: "+Cannang);
-        System.out.println("WebCam: "+Webcam);
-        System.out.println("Kich Thuoc: "+Kichthuoc);
+        System.out.println("Can Nang: "+CanNang);
+        System.out.println("WebCam: "+WebCam);
+        System.out.println("Kich Thuoc: "+KichThuoc);
     }
 }
