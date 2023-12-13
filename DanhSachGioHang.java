ac097af6ab9c9cf3758d698758a2fafd6c207698
TDKhoa2712 <147808726+TDKhoa2712@users.noreply.github.com>
2023-12-14 03:39:18 +0700

    Khoa up

--- DanhSachGioHang.java
+++ DanhSachGioHang.java
@@ -1,4 +1,4 @@
-package Class;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
--- DanhSachSanPham.java
+++ DanhSachSanPham.java
@@ -1,4 +1,4 @@
-package Class;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
