c6e3474f865c529bff6ad9948f776e494d48ff10
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2023-12-17 09:02:15 +0700

    1

--- Menu.java
+++ Menu.java
@@ -29,7 +29,7 @@ public class Menu {
                 break;
                 case 3:
                     dspn.docFileJava("PhieuNhap.txt");
-                    dspn.nhap();
+                    
                     dspn.thaoTac();
                 break;
                 case 4:
